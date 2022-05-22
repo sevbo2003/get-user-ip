3b80b0a423147a529d08e20702c860dd1cef2753
sevbo2003 <sevbofx@gmail.com>
2022-05-22 16:33:21 +0500

    Model added

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Information(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    client = models.TextField()
+    ip = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.ip
+
+    class Meta:
+        ordering = ['-created']
