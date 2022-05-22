069c0e85d2e161f6c7949826622fa3c12233463e
sevbo2003 <sevbofx@gmail.com>
2022-05-22 16:40:58 +0500

    View added

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,20 @@
 from django.shortcuts import render
+from .models import Information
+from django.http import HttpResponse
+
+
+def home(request):
+    def get_client_ip(request):
+        x = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x:
+            ip = x.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+    client_address = request.META.get("HTTP_USER_AGENT")
+    inf = Information()
+    inf.user = request.user
+    inf.client = client_address
+    inf.ip = get_client_ip(request)
+    inf.save()
+    return HttpResponse("Lalala")
